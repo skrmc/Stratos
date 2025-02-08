8849ef17a1a3d0ab9b351db894baa42a5ec2ca6a
05blue04 <alexmax.pienkowski@gmail.com>
2025-02-07 20:29:01 -0500

    chore(server): add dockerfile and docker compose

---
+++ server/src/config/database.ts
@@ -0,0 +1,13 @@
+import postgres from 'postgres' 
+import 'dotenv/config'
+
+
+const sql = postgres({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+})
+
+export default sql 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,24 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import sql from './config/database.js'
+import dotenv from 'dotenv'
+import { timeStamp } from 'console'
 
 const app = new Hono()
+
+app.get('/db-test', async (c) => {
+  try {
+    const result = await sql`SELECT NOW()`
+    return c.json({
+      status: 'Connected :D',
+      timestamp: result[0].NOW
+    })
+    } catch (error){
+      console.error('Db connection error:', error)
+      return c.json({error: 'Failed to connect to database'},500)
+    
+    }
+  })
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -10,7 +27,14 @@ app.get('/', (c) => {
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
 
+
 serve({
   fetch: app.fetch,
   port,
+  hostname: '0.0.0.0',
+  onListen: ({ hostname, port }) => {
+    console.log(`Server started on ${hostname}:${port}`);
+  },
 })
+  
+
